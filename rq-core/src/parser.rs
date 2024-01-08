ab5a4b49da893d44e75b741a32fb5432ad3896f7
TheRealLorenz <lollobellina@gmail.com>
2024-01-08 16:43:39 +0100

    Better grammar

--- rq-core/src/parser.rs
+++ rq-core/src/parser.rs
@@ -181,13 +181,17 @@ pub fn parse(input: &str) -> Result<HttpFile, Box<Error<Rule>>> {
 
 #[cfg(test)]
 mod tests {
+    use core::panic;
+
     use super::{parse, HttpFile};
     use reqwest::{Method, Version};
 
     fn assert_parses(input: &str) -> HttpFile {
         let parsed = parse(input);
-        assert!(parsed.is_ok());
-        parsed.unwrap()
+        match parsed {
+            Ok(parsed) => parsed,
+            Err(e) => panic!("{e}"),
+        }
     }
 
     #[test]
@@ -275,13 +279,13 @@ GET test.dev HTTP/1.0
     #[test]
     fn test_query_params() {
         let input = r#"
-POST test.dev?foo=bar&baz=2 HTTP/1.0
+POST test.dev?foo=bar&baz=2&fif=fof HTTP/1.0
 authorization: token
 
 "#;
         let file = assert_parses(input);
         assert_eq!(file.requests.len(), 1);
-        assert_eq!(file.requests[0].query.len(), 2);
+        assert_eq!(file.requests[0].query.len(), 3);
         assert_eq!(file.requests[0].query.get("foo").unwrap(), "bar");
         assert_eq!(file.requests[0].query.get("baz").unwrap(), "2");
     }
@@ -304,7 +308,7 @@ authorization: token
     fn test_multiline_query() {
         let input = r#"
 POST test.dev
-?foo=bar
+        ?foo=bar
         &baz=42 HTTP/1.0
 authorization: token
 
